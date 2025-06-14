fe3e40d514fcc9e0229fd8390013fafbba32d099
Rinat <pgr40@yandex.ru>
2025-06-14 22:43:00 +0300

    created MasterHumanoidCaracter

--- Source/ContrarySurvivor/Private/MasterHumanoidCharacter.cpp
+++ Source/ContrarySurvivor/Private/MasterHumanoidCharacter.cpp
@@ -1,35 +1,78 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MasterHumanoidCharacter.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ContrarySurvivor/ContrarySurvivor.h" // Исправленный include
 
-// Sets default values
 AMasterHumanoidCharacter::AMasterHumanoidCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+ 	PrimaryActorTick.bCanEverTick = true;
 
+    MaxHealth = 100.0f;
+    Health = MaxHealth;
+
+    bIsAttacking = false;
+
+    HeadMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("HeadMesh"));
+    HeadMesh->SetupAttachment(GetMesh());
+
+    TorsoMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("TorsoMesh"));
+    TorsoMesh->SetupAttachment(GetMesh());
+
+    LegsMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("LegsMesh"));
+    LegsMesh->SetupAttachment(GetMesh());
 }
 
-// Called when the game starts or when spawned
 void AMasterHumanoidCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	
+    GetCharacterMovement()->MaxWalkSpeed = 300.f;
 }
 
-// Called every frame
 void AMasterHumanoidCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void AMasterHumanoidCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+void AMasterHumanoidCharacter::Attack()
+{
+    UE_LOG(LogTemp, Warning, TEXT("MasterHumanoidCharacter: Attack!"));
+}
+
+float AMasterHumanoidCharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+    if (ActualDamage > 0.0f)
+    {
+        Health -= ActualDamage;
+        Health = FMath::Max(Health, 0.0f);
+        UE_LOG(LogTemp, Warning, TEXT("Health now %s"), *FString::SanitizeFloat(Health));
+
+        if (Health <= 0.f)
+        {
+             UE_LOG(LogTemp, Warning, TEXT("I am dead"));
+        }
+    }
+    return ActualDamage;
+}
+
+void AMasterHumanoidCharacter::RestoreHealth(float HealAmount)
+{
+    if (HealAmount <= 0.0f) return;
+
+    Health += HealAmount;
+    Health = FMath::Min(Health, MaxHealth);
+
+     UE_LOG(LogTemp, Warning, TEXT("MasterHumanoidCharacter: RestoreHealth! Health = %f"), Health);
+}
+
+void AMasterHumanoidCharacter::UpdateCharacterAppearance()
+{
+    UE_LOG(LogTemp, Warning, TEXT("MasterHumanoidCharacter: UpdateCharacterAppearance!"));
+}
--- Source/ContrarySurvivor/Public/MasterHumanoidCharacter.h
+++ Source/ContrarySurvivor/Public/MasterHumanoidCharacter.h
@@ -4,9 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+//#include "InventoryComponent.h" // Comented until i create InventoryComponent
+//#include "Weapon.h"
 #include "MasterHumanoidCharacter.generated.h"
 
-UCLASS()
+UCLASS(Abstract, Blueprintable)
 class CONTRARYSURVIVOR_API AMasterHumanoidCharacter : public ACharacter
 {
 	GENERATED_BODY()
@@ -18,7 +20,50 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats")
+    float Health;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats")
+    float MaxHealth;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+    bool bIsAttacking;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* HeadMesh; 
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* TorsoMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* LegsMesh; 
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	//UInventoryComponent* Inventory; 
+    //Inventory Comented Because i didnt created it yet
+
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	virtual void Attack(); // virtual UseWeapoon function To atack enemy. Whill be derived in child class
+
+    UFUNCTION(BlueprintCallable, Category = "Appearance")
+	void UpdateCharacterAppearance();
+
+	//UFUNCTION(BlueprintCallable, Category = "Equipment")
+    //virtual AWeapon* GetCurrentWeapon() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+    FORCEINLINE float GetHealth() const { return Health; }
+
+    UFUNCTION(BlueprintCallable, Category = "Stats")
+    FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Stats")
+	virtual void RestoreHealth(float HealAmount);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
